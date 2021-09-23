db6747adfeb798b379bf47be41f80ed1563590d6
YevaUsatova <yeva.usatova@gmail.com>
2021-09-23 15:41:09 -0400

    commit tree 4e9ea3cf3eb9777d19ccf523ff66e31ee158d186

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+class CashRegister
+    attr_accessor :total
+    def initialize (total = 0)
+        @total = total
+    end
+    def discount
+        self.total =20
+    end
 
+    def add_item (title, price)
+        @total = price      
+    end
+    
+    def apply_discount
+        if discount == true
+            @total = @total -100 / 20
+            "After the discount, the total comes to #{total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def items (item, price, count)
+        items =[]
+        if item == 2
+            items << item
+        else
+            item << item* count
+        end 
+    end
+
+    def void_last_transaction
+        new_array = @total.split
+        
+    end
+    
+end
